424667538c2a4c531acb6faac93ce1c59d5374e1
JorgeUPS <jchavezg3@est.ups.edu.ec>
2021-01-25 13:35:51 -0500

    Phyton

---
+++ 10_Integracion_input_string.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:13:06 2021
+
+@author: Bryan
+"""
+
+Nombre=input('Ingrese su nombre ')
+Apellido=input('Ingrese su Apellido')
+Ubicacion=input('Ingrese su Ubicacion ')
+Edad=input('Ingrese su edad ') 
+space=" "
+str1='olvídate de salones repletos de estudiantes'
+str2="aprende inglés a tu propio ritmo desde "
+str3="con el que lograrás fluidez en inglés a tus "
+str4='años'
+str5='en poco tiempo te llamarán teacher'
+
+print(Nombre,str1,str2,Ubicacion,str3,Edad,str4,str5,Apellido)
+print('\n')
+#print('hola',leer)
+
